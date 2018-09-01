48d1b1e8f44105d8a0f55f9bd079f1d5654489a3
PhoebeMango <slowlymango@sina.com>
2018-09-01 20:42:47 +0800

    Initial commit

---
+++ 18:09:01/twoSum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+	def twoSum (self, nums, target):
+		"""
+		type nums: List[int]
+		type target: int
+		"""
+		dic = dict{}
+		for index, value in enumerate (nums):
+			sub = target -value
+			if sub in dic:
+				return [dic[sub],index]
+			else:
+				dic[value] = index
